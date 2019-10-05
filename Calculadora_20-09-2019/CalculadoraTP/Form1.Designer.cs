1c075e16da97cb922b53cdc56ef19f29c8278ae1
sergiovic <sergiolaura04@gmail.com>
2019-10-04 21:42:47 -0300

    CalculadoraTP

---
+++ Calculadora_20-09-2019/CalculadoraTP/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CalculadoraTP
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculadora));
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividit = new System.Windows.Forms.Button();
+            this.btnRaiz = new System.Windows.Forms.Button();
+            this.btnEleva = new System.Windows.Forms.Button();
+            this.btnConcatenar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(12, 53);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 20);
+            this.txtN1.TabIndex = 0;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(147, 53);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 20);
+            this.txtN2.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 12);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(235, 29);
+            this.txtResultado.TabIndex = 2;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(12, 80);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 3;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(91, 80);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 4;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(172, 80);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 5;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividit
+            // 
+            this.btnDividit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividit.Location = new System.Drawing.Point(12, 109);
+            this.btnDividit.Name = "btnDividit";
+            this.btnDividit.Size = new System.Drawing.Size(75, 23);
+            this.btnDividit.TabIndex = 6;
+            this.btnDividit.Text = "/";
+            this.btnDividit.UseVisualStyleBackColor = true;
+            this.btnDividit.Click += new System.EventHandler(this.btnDividit_Click);
+            // 
+            // btnRaiz
+            // 
+            this.btnRaiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaiz.Location = new System.Drawing.Point(91, 109);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(75, 23);
+            this.btnRaiz.TabIndex = 7;
+            this.btnRaiz.Text = "RAIZ";
+            this.btnRaiz.UseVisualStyleBackColor = true;
+            this.btnRaiz.Click += new System.EventHandler(this.btnRaiz_Click);
+            // 
+            // btnEleva
+            // 
+            this.btnEleva.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEleva.Location = new System.Drawing.Point(172, 109);
+            this.btnEleva.Name = "btnEleva";
+            this.btnEleva.Size = new System.Drawing.Size(75, 23);
+            this.btnEleva.TabIndex = 8;
+            this.btnEleva.Text = "X eleva Y";
+            this.btnEleva.UseVisualStyleBackColor = true;
+            this.btnEleva.Click += new System.EventHandler(this.btnEleva_Click);
+            // 
+            // btnConcatenar
+            // 
+            this.btnConcatenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConcatenar.Location = new System.Drawing.Point(12, 138);
+            this.btnConcatenar.Name = "btnConcatenar";
+            this.btnConcatenar.Size = new System.Drawing.Size(118, 23);
+            this.btnConcatenar.TabIndex = 9;
+            this.btnConcatenar.Text = "Concatena";
+            this.btnConcatenar.UseVisualStyleBackColor = true;
+            this.btnConcatenar.Click += new System.EventHandler(this.btnConcatenar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(136, 138);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(111, 23);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(259, 172);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnConcatenar);
+            this.Controls.Add(this.btnEleva);
+            this.Controls.Add(this.btnRaiz);
+            this.Controls.Add(this.btnDividit);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmCalculadora";
+            this.Text = "CalculadoraTP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividit;
+        private System.Windows.Forms.Button btnRaiz;
+        private System.Windows.Forms.Button btnEleva;
+        private System.Windows.Forms.Button btnConcatenar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Calculadora_20-09-2019/CalculadoraTP/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraTP
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            int N1;
+            int N2;
+            int soma;
+            string rsoma;
+            try
+            {
+                N1 = Convert.ToInt16(txtN1.Text);
+                N2 = Convert.ToInt16(txtN2.Text);
+                soma = N1 + N2;
+                rsoma = Convert.ToString(soma);
+                txtResultado.Text = rsoma;
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Formatação Errada.");
+            }
+
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            int N1;
+            int N2;
+            int subtrai;
+            string rsubtrai;
+            try
+            {
+                N1 = Convert.ToInt16(txtN1.Text);
+                N2 = Convert.ToInt16(txtN2.Text);
+                subtrai = N1 - N2;
+                rsubtrai = Convert.ToString(subtrai);
+                txtResultado.Text = rsubtrai;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Formatação Errada.");
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int N1;
+            int N2;
+            int multiplica;
+            string rmultiplica;
+            try
+            {
+                N1 = Convert.ToInt16(txtN1.Text);
+                N2 = Convert.ToInt16(txtN2.Text);
+                multiplica = N1 * N2;
+                rmultiplica = Convert.ToString(multiplica);
+                txtResultado.Text = rmultiplica;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Formatação Errada.");
+            }
+        }
+
+        private void btnDividit_Click(object sender, EventArgs e)
+        {
+            double N1;
+            double N2;
+            double divide;
+            string rdivide;
+            try
+            {
+                N1 = Convert.ToDouble(txtN1.Text);
+                N2 = Convert.ToDouble(txtN2.Text);
+                divide = N1 / N2;
+                rdivide = Convert.ToString(divide);
+                txtResultado.Text = rdivide;
+            }
+            catch(DivideByZeroException)
+            {
+                MessageBox.Show("Não é possível dividir por Zero");
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Formatação Errada.");
+            }
+}
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtN1.Text = "";
+            txtN2.Text = "";
+            txtResultado.Text = "";
+        }
+
+        private void btnConcatenar_Click(object sender, EventArgs e)
+        {
+            string N1;
+            string N2;
+            string concatena;
+            N1 = Convert.ToString(txtN1.Text);
+            N2 = Convert.ToString(txtN2.Text);
+            concatena = N1 + N2;
+            txtResultado.Text = concatena;
+        }
+
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            double N1;
+            double N2;
+            double raiz;
+            string rraiz;
+            try
+            {
+                N1 = Convert.ToDouble(txtN1.Text);
+                raiz = Math.Sqrt(N1);
+                rraiz = Convert.ToString(raiz);
+                txtResultado.Text = rraiz;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Formatação Errada.");
+            }
+        }
+
+        private void btnEleva_Click(object sender, EventArgs e)
+        {
+            double N1;
+            double N2;
+            double eleva;
+            string releva;
+            try
+            {
+                N1 = Convert.ToDouble(txtN1.Text);
+                N2 = Convert.ToDouble(txtN2.Text);
+                eleva = Math.Pow(N1, N2);
+                releva = Convert.ToString(eleva);
+                txtResultado.Text = releva;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Formatação Errada.");
+            }
+        }
+    }
+}
